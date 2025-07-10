7d1419cebf81624cf77b99f8959c32a6b216340a
Liliia <liliya.golets@gmail.com>
2025-07-11 00:25:28 +0300

    second commit mistake correction

--- lesson2/HomeWork2.1.py
+++ lesson2/HomeWork2.1.py
@@ -1,8 +1,8 @@
-number = input("Enter the four digit integer:")
-thousands = int(number) // 1000
-hundreds = (int(number) % 1000) // 100
-tens = (int(number) % 100) // 10
-units = int(number) % 10
+number = int(input("Enter the four digit integer:"))
+thousands = number // 1000
+hundreds = (number % 1000) // 100
+tens = (number % 100) // 10
+units = number % 10
 print(thousands)
 print(hundreds)
 print(tens)
