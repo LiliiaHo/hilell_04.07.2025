e80add3c129a23c7d17166c08070a173582c77ac
Liliia <liliya.golets@gmail.com>
2025-07-11 02:35:07 +0300

    third commit mistake correction

---
+++ lesson2/HomeWork2.2(variantA).py
@@ -0,0 +1,9 @@
+abcde = int(input("Please enter the five digit integer:"))
+e = abcde % 10
+d = (abcde % 100) // 10
+c = (abcde % 1000) // 100
+b = (abcde % 10000) // 1000
+a = abcde // 10000
+edcba = (str(e) + str(d) + str(c) + str(b) + str(a))
+print (edcba)
+
