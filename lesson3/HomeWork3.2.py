39acf2f4b196bf11691fb254c12e64f8fe568864
Liliia <liliya.golets@gmail.com>
2025-07-17 16:34:46 +0300

    6th commit 3.2 correction

--- lesson3/HomeWork3.2.py
+++ lesson3/HomeWork3.2.py
@@ -1,36 +1,7 @@
 lst = [12, 3, 4, 10]
-if len(lst) <= 1:
-    print(lst)
-elif len(lst) > 1:
-    moved_element = lst.pop(-1)
-    lst.insert(0, moved_element)
-    print(lst)
-
-lst = [1]
-if len(lst) <= 1:
-    print(lst)
-elif len(lst) > 1:
-    moved_element = lst.pop(-1)
-    lst.insert(0, moved_element)
-    print(lst)
-
-lst = []
-if len(lst) <= 1:
-    print(lst)
-elif len(lst) > 1:
-    moved_element = lst.pop(-1)
-    lst.insert(0, moved_element)
-    print(lst)
-
-lst = [12, 3, 4, 10, 8]
-if len(lst) <= 1:
-    print(lst)
-elif len(lst) > 1:
-    moved_element = lst.pop(-1)
-    lst.insert(0, moved_element)
-    print(lst)
-
-lst = [0]
+# lst = [1]
+# lst = []
+# lst = [12, 3, 4, 10, 8]
 if len(lst) <= 1:
     print(lst)
 elif len(lst) > 1:
