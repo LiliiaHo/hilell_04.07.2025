76d2ee6ca21ed13b251eb1d4fb6c37acf333d06d
Liliia <liliya.golets@gmail.com>
2025-07-21 21:18:26 +0300

    8th commit 4.1

---
+++ lesson4/HomeWork4.1.py
@@ -0,0 +1,17 @@
+lst = [0, 1, 0, 12, 3]
+# lst = [0]
+# lst = [1, 0, 13, 0, 0, 0, 5]
+# lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+lst_without_0 = []
+lst_0 = []
+for element in lst:
+    if element > 0:
+        lst_without_0.append(element)
+    elif element == 0:
+        lst_0.append(element)
+new_list = lst_without_0 + lst_0
+print(new_list)
+
+
+
+
