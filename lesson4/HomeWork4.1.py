4fb4e7b288699e67ec34bc0e0b9df14aebf53986
Liliia <liliya.golets@gmail.com>
2025-07-22 17:20:04 +0300

    8th commit 4.1 mistake correction

--- lesson4/HomeWork4.1.py
+++ lesson4/HomeWork4.1.py
@@ -2,13 +2,13 @@
 # lst = [0]
 # lst = [1, 0, 13, 0, 0, 0, 5]
 lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-new_lst = []
+zeros= []
 for i in lst:
     if i == 0:
-        new_lst.append(i)
+        zeros.append(i)
 while 0 in lst:
     lst.remove(0)
-lst.extend(new_lst)
+lst.extend(zeros)
 print(lst)
 
 
