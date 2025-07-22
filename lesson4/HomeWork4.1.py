e31f314ab5eb4b9cc69b420553ea0a20ae3c5f86
Liliia <liliya.golets@gmail.com>
2025-07-22 17:05:30 +0300

    8th commit 4.1 mistake correction

--- lesson4/HomeWork4.1.py
+++ lesson4/HomeWork4.1.py
@@ -1,16 +1,15 @@
-lst = [0, 1, 0, 12, 3]
+# lst = [0, 1, 0, 12, 3]
 # lst = [0]
 # lst = [1, 0, 13, 0, 0, 0, 5]
-# lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-lst_without_0 = []
-lst_0 = []
+lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+new_lst = []
 for i in lst:
-    if i > 0:
-        lst_without_0.append(i)
-    elif i == 0:
-        lst_0.append(i)
-new_list = lst_without_0 + lst_0
-print(new_list)
+    if i == 0:
+        new_lst.append(i)
+while 0 in lst:
+    lst.remove(0)
+lst.extend(new_lst)
+print(lst)
 
 
 
