af7881608fd3541c3aef2e8babd24e22fbf17254
Liliia <liliya.golets@gmail.com>
2025-08-02 01:06:22 +0300

    14 commit 8.3

---
+++ lesson8/HomeWork8.3.py
@@ -0,0 +1,8 @@
+def find_unique_value(lst):
+    for el in lst:
+      if lst.count(el) == 1:
+          return el
+assert find_unique_value([1, 2, 1, 1]) == 2, "Test1"
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, "Test2"
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, "Test3"
+print("OK")
