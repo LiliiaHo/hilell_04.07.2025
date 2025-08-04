ce5e96dd0f005bab8bfd3f0fcd7a8ab3ff537887
Liliia <liliya.golets@gmail.com>
2025-08-04 19:17:11 +0300

    16 commit 7.1 mistake correction

--- lesson7/HomeWork7.1.py
+++ lesson7/HomeWork7.1.py
@@ -1,5 +1,7 @@
-def say_hi(name, age):
+def say_hi(name: str, age: int) -> str:
     return f"Hi. My name is {name} and I am {age} years old"
+
+
 assert say_hi("Alex",32) == "Hi. My name is Alex and I am 32 years old", "Test1"
 assert say_hi("Frank",68) == "Hi. My name is Frank and I am 68 years old", "Test2"
 print("OK")
