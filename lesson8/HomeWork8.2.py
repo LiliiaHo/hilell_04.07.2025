05b9b7eb7277e38a0aaac3fbe482e5148459293b
Liliia <liliya.golets@gmail.com>
2025-08-19 19:25:00 +0200

    22 commit 8.2

---
+++ lesson8/HomeWork8.2.py
@@ -0,0 +1,14 @@
+def is_palindrome(text: str) -> bool:
+
+    only_text = [ch.lower() for ch in text if ch.isalnum()]
+    reversed_list = only_text[::-1]
+
+    return reversed_list == only_text
+
+
+assert is_palindrome("A man, a plan, a canal: Panama") is True, "Test1"
+assert is_palindrome("QP") is False, "Test2"
+assert is_palindrome("a") is True, "Test3"
+assert is_palindrome("aurora") is False, "Test4"
+
+print("OK")
